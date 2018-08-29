4aa55cc5e6ddbb86ef2513324fbee1f005858b99
csabi <nemeth.csaba@revolve.hu>
2018-08-30 00:35:54 +0200

    pycharmból feltöltve

--- ujfile.py
+++ ujfile.py
@@ -7,3 +7,29 @@ print "na ez egy diff"
 # TODO ez itt egy
 
 # todo ez is egy tudu!!
+
+
+from os import listdir
+from os import walk
+
+mypath = "f:\Temp"
+
+efiles = listdir(mypath)
+
+f = []
+maxFilename = 0
+maxFile = ""
+
+for (dirpath, dirnames, filenames) in walk(mypath):
+    for files in filenames:
+        # print files, len(files)
+        if len(files) > maxFilename:
+            maxFilename = len(files)
+            maxFile = files
+
+    # f.append(filenames)
+
+print 'Leghosszabb filenév hossza: \033[1;37;30m %d \033[0;31;30m karakter, a file neve: "%s"' % (maxFilename, maxFile)
+
+# print type(maxFile)
+
