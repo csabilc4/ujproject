ebd206775141178e295099756b6c56b8146565c5
csabi <nemeth.csaba@revolve.hu>
2018-09-04 09:25:38 +0200

    pycharmból feltöltve
    (cherry picked from commit 9bf50c5478498ddfcfab2f432c39b0515c33aa21)

--- ujmodul.py
+++ ujmodul.py
@@ -5,3 +5,23 @@ import math
 # print '\n'.join(sys.modules)
 # print math.cos(math.radians(60))
 
+# ff = [1,2,3,1]
+# gg = [1,2,3,1,3,3,1]
+# d = set(ff)
+# print d
+
+gyumolcsok = ["alma","körte"]
+penztarcaban = 22
+
+
+def kosar_kiirasa():
+    print gyumolcsok
+    gyumolcsok.append('barack')
+    print gyumolcsok
+
+    penztarcaban = 33
+    print penztarcaban
+
+kosar_kiirasa()
+print penztarcaban
+print gyumolcsok
