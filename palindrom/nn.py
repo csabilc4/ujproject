b3eb831f4784ef7046a41e351d857d6d85585a3d
csabi <nemeth.csaba@revolve.hu>
2018-09-06 00:18:56 +0200

    pycharmból feltöltve
    (cherry picked from commit 9bf50c5478498ddfcfab2f432c39b0515c33aa21)

--- palindrom/nn.py
+++ palindrom/nn.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import os
+import time
 
-_sourceFileName1 = 'e:\palin.txt'
-_sourceFileName1 = 'e:\HUN_ENG_dict.txt'
-_targetFileName1 = 'e:\HUN_ENG_dict TARGET2.txt'
+# os.chdir('e:/')
+
+currentDate = time.strftime(" %Y.%m.%d-%H:%M:%S")
+
+print os.getcwd()
+
+# _sourceFileName1 = '/palin.txt'
+_sourceFileName1 = 'HUN_ENG_dict TARGET short.txt'
+
+# _targetFile = open(_targetFileName1, 'w')
 
 def addLangCode():
-  _targetFile = open(_targetFileName1, 'w')
 
   try:
     _sourceFile = open(_sourceFileName1, 'r')
   except:
-    print 'Nincs ilyen fájl!'
+    print 'Nincs ilyen SOURCE fájl!'
     exit()
 
   blankLine = 1
@@ -35,57 +43,78 @@ def addLangCode():
   _sourceFile.close()
   _targetFile.close()
 
+lettersToDelete   = ["'", "-", " ", ".", "&", "*", "<", ">",
+                    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 def searchForPalindromWords():
   leghosszabb = 0
   ujszo = ''
   i = 0
   palinNum = 0
+  palinWords = []
 
-  _targetFileName1 = 'e:\palin_dict.txt'
+  bDeleteFirstLetter = 0
+  newLine = ''
 
   try:
-    _file = open(_targetFileName1, 'r')
+    _file = open(_sourceFileName1, 'r')
   except:
-    print 'Nincs ilyen fájl!'
+    print 'Nincs ilyen SOURCE fájl!'
     exit()
 
-  for line in _file:
-    if line.startswith('HUN'):
-      hunWord1 = line.replace('HUN:','')
-      hunWord2 = hunWord1.replace(' ','')
-      hunWord3 = hunWord2.replace('-','')
-      # print hunWord3
+  for _line in _file:
+    invalidLine = 0
 
-      i = 0
-      ujszo = ''
-      for chars in hunWord3:
-        # ujszo += hunWord3[len(hunWord3) - 1 - i]
-        ujszo = hunWord3[::-1]
-        i += 1
+    # _line = _line.lower()
+    line = (_line).rstrip("\n")
 
-        # if hunWord3 == ujszo and len(hunWord3) > 1:
-      print hunWord3, hunWord3[1], len(ujszo)
-          # palinNum += 1
+    for letter in lettersToDelete:
+        if letter in line:
+            # print letter
+            invalidLine = 1
+            break
 
-    # szo = ''
-    # for _szo in words:
-    #   hossz = len(szo)
-    #   szo = _szo.replace('.','')
-    #   i = 0
-    #   ujszo = ''
-    #   for chars in szo:
-    #     ujszo += szo[len(szo) - 1 - i]
-    #     i += 1
-    #
-    #     #     if szo == ujszo and len(szo) > 1:
-    #   #     ujszo = ujszo.encode('utf-8')
-    #   if szo == ujszo and len(szo) > 1:
-    #     print szo, ujszo
-    #     palinNum += 1
-  print 'Palindrom szavak száma: ', palinNum
-  #  print leghosszabb
+    if line.startswith('HUN') and len(line) > 6 and not invalidLine:
+      bDeleteFirstLetter = 0
+      for letter in lettersToDelete:
+          # print line[4]
+          if line[4] == letter:
+              bDeleteFirstLetter = 1
+
+      newLine = line[4:]
+      # newLine = line
+      # print newLine, len(newLine)
+
+      reverseLine = ''
+      # for letters in newLine:
+      #   reverseLine = newLine[::-1]
+
+      reverseLine = newLine[::-1]
+
+      print newLine, len(newLine), reverseLine, len(reverseLine)
+
+      if newLine.lower() == reverseLine.lower():
+        print newLine, reverseLine
+        palinNum += 1
+        palinWords.append(newLine)
+
+  print 'Palindrom szavak:', ", ".join(palinWords), #, palinNum
 
   _file.close()
 
-# addLangCode()
-searchForPalindromWords()
+def main():
+    # addLangCode()
+    searchForPalindromWords()
+    pass
+
+## Testing
+if __name__ == '__main__':
+    # print 'test OK!'
+
+    main()
+
+    # try:
+    #     main()
+    # except:
+    #     quit("Something went wrong!")
+
--- ujmodul.py
+++ ujmodul.py
@@ -2,11 +2,12 @@
 # coding=utf-8
 
 import math
+
 # print '\n'.join(sys.modules)
 # print math.cos(math.radians(60))
 
-ff = [1,2,3,1]
-gg = [1,2,3,1,3,3,1]
+ff = [1, 2, 3, 1]
+gg = [1, 2, 3, 1, 3, 3, 1]
 # d = set(ff)
 # print d
 
@@ -14,15 +15,42 @@ import sys
 
 # print '\n'.join(sys.path)
 
-print dir()
+# print ('ajtó\n')
+# print repr('ajtó\n')
+# print str(repr('ajtó\n'))
+
+# nevek = ['Németh Csaba', 'Németh Józsefné']
+# cimek = ['Szentendre', 'Mosonmagyaróvár']
+# for nev in range(len(nevek)):
+#     print nevek[nev].rjust(20 + nev * 0), cimek[nev].rjust(20 + nev * 0)
+# print '\n',
+# for nev in range(len(nevek)):
+#     print '%20s %20s' % (nevek[nev], cimek[nev])
+
+
+
+
+
+
+dd = 'ave'
+print len(dd)
+
+
+
+
+
+
+
 
 exit()
 
-gyumolcsok = ["alma","korte"]
+gyumolcsok = ["alma", "korte"]
 penztarcaban = 22
+
 
 def kiir():
     print gyumolcsok
+
 
 def kosar_kiirasa():
     print gyumolcsok
@@ -34,6 +62,7 @@ def kosar_kiirasa():
 
     # penztarcaban = 33
     print penztarcaban, id(penztarcaban)
+
 
 kosar_kiirasa()
 
